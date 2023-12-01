d4e22acefbc21f6673fc9aaacd3840d24dcaddb5
wesley <106327635+wfs1357@users.noreply.github.com>
2023-12-01 09:33:19 -0300

    hhjhh

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ function tocaSom(idElementoAudio) {
     document.querySelector(idElementoAudio).play();
 }
 
- const listaDeTeclas = document.querySelectorAll(".tecla");
+      const listaDeTeclas = document.querySelectorAll(".tecla");
 
  for (let contador = 0; contador < listaDeTeclas.length; contador++) {
     const tecla = listaDeTeclas[contador];
